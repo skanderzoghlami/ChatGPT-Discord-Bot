ce03a1785986bc22f38bed9b95b07fe44be21835
skanderzoghlami <skander.zoghlami@insat.u-carthage.tn>
2023-03-09 11:40:09 +0100

    finished the stable version

--- commands/robin.js
+++ commands/robin.js
@@ -1,10 +1,46 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { openai_token } = require('../config.json');
+const { Configuration, OpenAIApi } = require("openai");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('robin')
-		.setDescription('Give you some answers!'),
+		.setDescription('Give you some answers!')
+		.addStringOption(option =>
+			option
+				.setName('question')
+				.setDescription('The question')
+				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Answer!');
+		await interaction.deferReply();
+		answer = 'hmmm';
+		const configuration = new Configuration({
+			apiKey: openai_token,
+		});
+		const openai = new OpenAIApi(configuration);
+
+		const prompt = interaction.options.getString('question');
+
+		const GPT35TurboMessage = [
+			{ role: "system", content: `You are nico robin from one piece.` },
+			{ role: "user", content: prompt },
+		  ];
+
+
+
+
+		let GPT35Turbo = async (message) => {
+			const response = await openai.createChatCompletion({
+			  model: "gpt-3.5-turbo",
+			  messages: message,
+			});
+		  
+			interaction.editReply(response.data.choices[0].message.content);
+		  };
+
+
+		await GPT35Turbo(GPT35TurboMessage);
+
+
 	},
 };
--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token , openai_token } = require('./config.json');
+const tectalicOpenai = require('@tectalic/openai').default;
 
-// Create a new client instance
+
+
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
 });
 
-// Log in to Discord with your client's token
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+	const openaiClient = tectalicOpenai(openai_token);
+	if (command === 'robin') {
+		await interaction.deferReply();
+		openaiClient.completions.create({
+			model: 'text-davinci-003',
+			prompt: interaction.options.getString('question')
+		  }).then((response) => {
+			answer = response.data.choices[0].text.trim();
+		})
+		await interaction.editReply(answer);
+	}
+
+
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 client.login(token);
